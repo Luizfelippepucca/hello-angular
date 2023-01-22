1589c023bb54501a5a53ebb8d763dbb3b6957dff
luiz pucca <lu1z_21@hotmail.com>
2023-01-22 00:47:17 -0300

    create routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { CursosDetalhesComponent } from './cursos-detalhes/cursos-detalhes.component';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'home', component: CursosDetalhesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-import { LoginModule } from './login/login.module';
 import { DataBindingComponent } from './data-binding/data-binding.component';
 
 @NgModule({
   declarations: [AppComponent, DataBindingComponent],
   providers: [],
   bootstrap: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, LoginModule],
+  imports: [BrowserModule, AppRoutingModule],
 })
 export class AppModule {}
